c0e93d9bb3d335e76075ae0945baba4bebc94fc1
kn0 <vladvatafu9@gmail.com>
2019-08-10 07:41:48 +0300

    Add files via upload
    Random stuff :))

---
+++ Game_Engine/SoftEngine.js
@@ -0,0 +1,82 @@
+var SoftEngine;
+(function (SoftEngine) {
+    var Camera = (function () {
+        function Camera() {
+            this.Position = BABYLON.Vector3.Zero();
+            this.Target = BABYLON.Vector3.Zero();
+        }
+        return Camera;
+    })();
+    SoftEngine.Camera = Camera;    
+    var Mesh = (function () {
+        function Mesh(name, verticesCount) {
+            this.name = name;
+            this.Vertices = new Array(verticesCount);
+            this.Rotation = BABYLON.Vector3.Zero();
+            this.Position = BABYLON.Vector3.Zero();
+        }
+        return Mesh;
+    })();
+    SoftEngine.Mesh = Mesh;    
+    var Device = (function () {
+        function Device(canvas) {
+            this.workingCanvas = canvas;
+            this.workingWidth = canvas.width;
+            this.workingHeight = canvas.height;
+            this.workingContext = this.workingCanvas.getContext("2d");
+        }
+        Device.prototype.clear = function () {
+            this.workingContext.clearRect(0, 0, this.workingWidth, this.workingHeight);
+            this.backbuffer = this.workingContext.getImageData(0, 0, this.workingWidth, this.workingHeight);
+        };
+        Device.prototype.present = function () {
+            this.workingContext.putImageData(this.backbuffer, 0, 0);
+        };
+        Device.prototype.putPixel = function (x, y, color) {
+            this.backbufferdata = this.backbuffer.data;
+            var index = ((x >> 0) + (y >> 0) * this.workingWidth) * 4;
+            this.backbufferdata[index] = color.r * 255;
+            this.backbufferdata[index + 1] = color.g * 255;
+            this.backbufferdata[index + 2] = color.b * 255;
+            this.backbufferdata[index + 3] = color.a * 255;
+        };
+        Device.prototype.project = function (coord, transMat) {
+            var point = BABYLON.Vector3.TransformCoordinates(coord, transMat);
+            var x = point.x * this.workingWidth + this.workingWidth / 2.0 >> 0;
+            var y = -point.y * this.workingHeight + this.workingHeight / 2.0 >> 0;
+            return (new BABYLON.Vector2(x, y));
+        };
+        Device.prototype.drawPoint = function (point) {
+            if(point.x >= 0 && point.y >= 0 && point.x < this.workingWidth && point.y < this.workingHeight) {
+                this.putPixel(point.x, point.y, new BABYLON.Color4(1, 1, 0, 1));
+            }
+        };
+        Device.prototype.drawLine = function (point0, point1) {
+            var dist = point1.subtract(point0).length();
+            if(dist < 2) {
+                return;
+            }
+            var middlePoint = point0.add((point1.subtract(point0)).scale(0.5));
+            this.drawPoint(middlePoint);
+            this.drawLine(point0, middlePoint);
+            this.drawLine(middlePoint, point1);
+        };
+        Device.prototype.render = function (camera, meshes) {
+            var viewMatrix = BABYLON.Matrix.LookAtLH(camera.Position, camera.Target, BABYLON.Vector3.Up());
+            var projectionMatrix = BABYLON.Matrix.PerspectiveFovLH(0.78, this.workingWidth / this.workingHeight, 0.01, 1.0);
+            for(var index = 0; index < meshes.length; index++) {
+                var cMesh = meshes[index];
+                var worldMatrix = BABYLON.Matrix.RotationYawPitchRoll(cMesh.Rotation.y, cMesh.Rotation.x, cMesh.Rotation.z).multiply(BABYLON.Matrix.Translation(cMesh.Position.x, cMesh.Position.y, cMesh.Position.z));
+                var transformMatrix = worldMatrix.multiply(viewMatrix).multiply(projectionMatrix);
+                for (var i = 0; i < cMesh.Vertices.length -1; i++){
+                    var point0 = this.project(cMesh.Vertices[i], transformMatrix);
+                    var point1 = this.project(cMesh.Vertices[i + 1], transformMatrix);
+                    this.drawLine(point0, point1);
+                }
+            }
+        };
+        
+        return Device;
+    })();
+    SoftEngine.Device = Device;    
+})(SoftEngine || (SoftEngine = {}));
